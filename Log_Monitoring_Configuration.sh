1f49ae3241e67ab047b7e2b0579397b44cb2f2bd
sathish-gopi <130130987+sathish-gopi@users.noreply.github.com>
2023-04-07 16:57:08 +0530

    Update Log_Monitoring_Configuration.sh

--- Log_Monitoring_Configuration.sh
+++ Log_Monitoring_Configuration.sh
@@ -4,7 +4,7 @@ sudo touch /var/log/clipfilp/vps.log
 cd ~/
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i amazon-cloudwatch-agent.deb
-sudo mv log_monitoring/config.json /opt/aws/amazon-cloudwatch-agent/bin/
+sudo mv  ~/monitoring/log_monitoring/config.json /opt/aws/amazon-cloudwatch-agent/bin/
 sudo chmod 777 /opt/aws/amazon-cloudwatch-agent/bin/config.json
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
 curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
